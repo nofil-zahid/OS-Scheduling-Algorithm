2a8501fa00affe8a9219ee61f86f0035dfea13cf
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2024-08-18 00:10:21 +0500

    imports + inputs

--- src/Main.java
+++ src/Main.java
@@ -7,40 +7,50 @@ public class Main {
 
         // ========================= FCFS ========================
 
-//        Process[] p = {
-//          new Process(0, 7, 8),
-//          new Process(1, 10, 13),
-//          new Process(2, 18, 25),
-//          new Process(3, 27, 28),
+//        Process[] p1 = {
+//          new Process(idCount++, 7, 8),
+//          new Process(idCount++, 10, 13),
+//          new Process(idCount++, 18, 25),
+//          new Process(idCount++, 27, 28),
 //        };
 
-//        Process[] p = {
-//            new Process(1,2,8),
-//            new Process(2,4,2),
-//            new Process(3,1,1),
-//            new Process(4,3,4),
-//            new Process(5,6,3)
+//        Process[] p2 = {
+//            new Process(idCount++,2,8),
+//            new Process(idCount++,4,2),
+//            new Process(idCount++,1,1),
+//            new Process(idCount++,3,4),
+//            new Process(idCount++,6,3)
 //        };
 
-//        Process[] p = {
-//                new Process(1, 0, 1),
-//                new Process(2, 3, 2),
-//                new Process(3, 4, 3)
+//        Process[] p3 = {
+//                new Process(idCount++, 0, 1),
+//                new Process(idCount++, 3, 2),
+//                new Process(idCount++, 4, 3)
 //        };
-
+//
 //        FCFS algo = new FCFS(p);
 
         // ======================== SJF ========================
 
-        Process[] p = {
-          new Process(idCount++, 2,8),
-          new Process(idCount++, 4,2),
-          new Process(idCount++, 1,5),
-          new Process(idCount++, 3,4),
-          new Process(idCount++, 6,3),
+        Process[] p1 = {
+                new Process(idCount++, 0, 7),
+                new Process(idCount++, 2, 4),
+                new Process(idCount++, 4, 1),
+                new Process(idCount++, 5, 4)
         };
 
-        SJF algo = new SJF(p);
+//        Process[] p2 = {
+//                new Process(idCount++, 0, 5),
+//                new Process(idCount++, 8, 3),
+//                new Process(idCount++, 12, 6),
+//                new Process(idCount++, 15, 4)
+//        };
+
+        SJF algo = new SJF(p1);
+
+        // ======================== SRTF ========================
+        // ======================== PRIORITY SCHDULING ========================
+        // ======================== ROUND ROBIN ========================
 
         System.out.println("QUESTION :_");
         algo.printArrayInTable();
