28e466409a61c804ad11f9ac342eb486412347f8
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2024-08-17 22:09:17 +0500

    console based gant chart

--- src/GanttChart.java
+++ src/GanttChart.java
@@ -1,3 +1,47 @@
 public class GanttChart {
-    
+    private int id;
+    private int start_time;
+    private int end_time;
+
+    public GanttChart(int id, int start_time, int end_time) {
+        this.id = id;
+        this.start_time = start_time;
+        this.end_time = end_time;
+    }
+    public GanttChart() {}
+
+    // ---------------------------- setter
+
+    public void setEndTime(int end_time) {
+        this.end_time = end_time;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStartTime(int start_time) {
+        this.start_time = start_time;
+    }
+
+    // ----------------------------- getter
+
+    public int getId() {
+        return id;
+    }
+
+    public int getEndTime() {
+        return end_time;
+    }
+
+    public int getStartTime() {
+        return start_time;
+    }
+
+    // ------------------------------------------ some important functions
+
+    @Override
+    public String toString() {
+        return "[P"+id+"=> start:"+start_time+", end:"+end_time+"]";
+    }
 }
